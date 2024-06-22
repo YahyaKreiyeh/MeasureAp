0a1cb319aec37d3f52b314fe407038799a99b5e5
Yahya Kreiyeh <yahyakreiyeh@gmail.com>
2024-06-22 12:27:38 +0300

    comment deletion

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,7 @@ import 'package:measureapp/measure_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
   await initiateGetIt();
-  // await ScreenUtil.ensureScreenSize();
-  // await EasyLocalization.ensureInitialized();
-  // FlutterNativeSplash.remove();
-  // EasyLocalization.logger.enableBuildModes = [];
   runApp(
     MeasureApp(
       appRouter: AppRouter(),
--- lib/measure_app.dart
+++ lib/measure_app.dart
@@ -15,15 +15,6 @@ class MeasureApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // var kColorScheme = ColorScheme.fromSeed(
-    //   seedColor: const Color.fromARGB(255, 96, 59, 181),
-    // );
-
-    // var kDarkColorScheme = ColorScheme.fromSeed(
-    //   brightness: Brightness.dark,
-    //   seedColor: const Color.fromARGB(255, 5, 99, 125),
-    // );
-
     return ScreenUtilInit(
       designSize: const Size(375, 812),
       minTextAdapt: true,
@@ -39,59 +30,9 @@ class MeasureApp extends StatelessWidget {
             ),
             primaryColor: ColorsManager.primary,
             scaffoldBackgroundColor: ColorsManager.scaffoldBackground,
-            // colorScheme: ColorScheme.fromSeed(
-            //   brightness: Brightness.light,
-            //   seedColor: ColorsManager.primary,
-            //   surface: ColorsManager.white,
-            //   surfaceTint: ColorsManager.white,
-            // ),
           ),
-          // darkTheme: ThemeData.dark().copyWith(
-          //   colorScheme: kDarkColorScheme,
-          //   cardTheme: const CardTheme().copyWith(
-          //     color: kDarkColorScheme.secondaryContainer,
-          //     margin: const EdgeInsets.symmetric(
-          //       horizontal: 16,
-          //       vertical: 8,
-          //     ),
-          //   ),
-          //   elevatedButtonTheme: ElevatedButtonThemeData(
-          //     style: ElevatedButton.styleFrom(
-          //       backgroundColor: kDarkColorScheme.primaryContainer,
-          //       foregroundColor: kDarkColorScheme.onPrimaryContainer,
-          //     ),
-          //   ),
-          // ),
-          // theme: ThemeData().copyWith(
-          //   colorScheme: kColorScheme,
-          //   appBarTheme: const AppBarTheme().copyWith(
-          //     backgroundColor: kColorScheme.onPrimaryContainer,
-          //     foregroundColor: kColorScheme.primaryContainer,
-          //   ),
-          //   cardTheme: const CardTheme().copyWith(
-          //     color: kColorScheme.secondaryContainer,
-          //     margin: const EdgeInsets.symmetric(
-          //       horizontal: 16,
-          //       vertical: 8,
-          //     ),
-          //   ),
-          //   elevatedButtonTheme: ElevatedButtonThemeData(
-          //     style: ElevatedButton.styleFrom(
-          //       backgroundColor: kColorScheme.primaryContainer,
-          //     ),
-          //   ),
-          //   textTheme: ThemeData().textTheme.copyWith(
-          //         titleLarge: TextStyle(
-          //           fontWeight: FontWeight.normal,
-          //           color: kColorScheme.onSecondaryContainer,
-          //           fontSize: 14,
-          //         ),
-          //       ),
-          // ),
-          // themeMode: ThemeMode.dark,
           debugShowCheckedModeBanner: false,
           initialRoute: Routes.navigationScreen,
-          // initialRoute: Routes.assessmentScreen,
           onGenerateRoute: appRouter.generateRoute,
         );
       },
